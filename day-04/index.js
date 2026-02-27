bf2b3e484936c1c20792331c98bba95b42f59ccb
shohan89 <your-maffin.killer007@gmail.com>
2026-02-27 17:23:47 +0600

    Complete Task 01

---
+++ day-04/index.js
@@ -0,0 +1,40 @@
+/**
+2. Build an ATM Cash Withdrawal System
+Rajan goes to the Axis bank ATM. He enters an amount to withdraw. The ATM only allows multiples of 100. Print “Withdrawal successful” if valid, otherwise print “Invalid amount”.
+
+3. Build a Calculator with switch-case
+Write a simple calculator that takes an operator (+, -, , /, %) as input, and performs the operation on two numbers. Print the output on the console.
+
+4. Pay for your movie ticket
+Imagine, the INOX charges ticket prices based on age:
+
+Children (<18 years): $3
+Adults (18 - 60 years): $10
+Seniors (60+ years): $8
+Write a program that prints the ticket price based on the person’s age.
+
+5. Horoscope Sign Checker
+Write a program that prints the zodiac sign(Aries, Taurus, Gemini, etc.) based on a person’s birth month. Make it month bases, not date based. Like March and April borns are Aries, Aplil and May born are Taurus, and so on. Do not use if-else.
+
+6. Which Triangle?
+A triangle has 3 sides. A Triangle type is determined by its sides:
+
+All sides equal is called, Equilateral Triangle.
+Two sides equal is called, Isosceles Triangle.
+All sides different is called, Scalene Triangle.
+Take the sides of a triangle as input and write a program to determine the triangle type. Change the inputs everytime manually to see if the output changes correctly.
+ */
+
+/********* Solution 1: Switch Case for Day of the Week *********/
+let day = "Monday";
+
+switch (day) {
+  case "monday":
+    console.log("It's the start of the week.");
+    break;
+  default:
+    console.log("It's a normal day.");
+}
+/**
+ * Output: "It's a normal day. Because the switch case is checking for "monday" in lowercase, but the variable 'day' is assigned "Monday" with an uppercase 'M'. Since there is no match, it goes to the default case and prints "It's a normal day."
+ */
